75b71b64a10502a17579f8d5f46f58b156727301
Leo310 <leodev310@gmail.com>
2022-02-06 13:32:54 +0100

    Add scroll looping (NOT FINISHED)

--- src/components/loopcontent.js
+++ src/components/loopcontent.js
@@ -1,22 +1,55 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Name from "./name";
 
 import globalstateobj from "../globalstate";
 
 class Loopcontent extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.lastScroll = 0;
+    this.clone = React.createRef();
+  }
+  componentDidMount() {
+    // this.cloneHeight = this.clone.current.style.height;
+    // this.cloneHeight = ReactDOM.findDOMNode(this.clone.current).clientHeight;
+    this.cloneHeight = 66 * 4;
+    this.loop = document.getElementById("loopcontent");
+    this.loop.scrollTop = 1; // to allow upwards scrolling
+    this.scrollHeight = this.loop.scrollHeight;
+    this.disableScroll = false;
+  }
+  scrollCheck() {
+    console.log(this.loop.scrollTop, ",  ", this.scrollHeight);
+    if (!this.disableScroll) {
+      let scrollTop = this.loop.scrollTop;
+      if (scrollTop > 1)
+        globalstateobj.scrollPosition += scrollTop - this.lastScroll;
+      this.lastScroll = scrollTop;
+
+      if (scrollTop + this.cloneHeight >= this.scrollHeight) {
+        // scroll to top and set to 1 to avoid looping
+        this.loop.scrollTop = 1;
+        this.disableScroll = true;
+      } else if (scrollTop <= 0) {
+        // scroll to bottom
+        this.loop.scrollTop = this.scrollHeight - this.cloneHeight;
+        this.disableScroll = true;
+      }
+    }
+    if (this.disableScroll) {
+      // disable scroll jumping for a short time to avoid flickering
+      window.setTimeout(() => {
+        this.disableScroll = false;
+      }, 40);
+    }
+  }
 
   render() {
     return (
       <>
         <div id="loopcover"></div>
-        <div
-          onScroll={(e) => (globalstateobj.scrollPosition = e.target.scrollTop)}
-          id="loopcontent"
-        >
-          <Name />
+        <div onScroll={(e) => this.scrollCheck(e)} id="loopcontent">
           <br />
           <p
             style={{ textAlign: "center", color: "#8be9fd", fontSize: "1.3vw" }}
@@ -84,6 +117,9 @@ class Loopcontent extends React.Component {
             accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
             no sea takimata sanctus est Lorem ipsum dolor sit amet.
           </p>
+          <Name ref={this.clone} />
+          <Name />
+          <Name />
           <Name />
         </div>
       </>
