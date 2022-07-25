b0a11ede97d329da4eda96e3aefeca396185fe4f
Leo310 <leodev310@gmail.com>
2022-07-25 20:17:05 +0200

    Add: nice arrow scroll feature

--- src/components/loopcontent.js
+++ src/components/loopcontent.js
@@ -5,14 +5,14 @@ import globalstateobj from "../globalstate";
 import ImmortalizerForm from "./immortalizerform"
 
 class Loopcontent extends React.Component {
-  state = {
-    active: "" 
-  }
+  state = {active : ""};
   constructor(props) {
     super(props);
     this.lastScroll = 0;
-    this.timer =null;
+    this.timer = null;
     this.onscroll = true;
+    this.onfirstscroll = true;
+    this.notfirstreloadscroll = false;
   }
   componentDidMount() {
     this.clones = document.getElementsByClassName("isclone");
@@ -24,25 +24,31 @@ class Loopcontent extends React.Component {
     // if scrolled in loopcontent, dont want to trigger wheel event in body.
     // thats why second listener on wheel which stops propagation and set to
     // true
-    this.loop.addEventListener("scroll",() => {
+    this.loop.addEventListener("scroll", () => {
       this.scrollCheck();
-      if(this.onscroll) {
-        this.setState({"active": "on"})
-        this.onscroll=false;
+      // scroll gets executed on page load somewhy
+      if (this.onscroll && this.notfirstreloadscroll) {
+        this.setState({"active" : "on"});
+        // if(this.onfirstscroll) {
+        //   document.getElementById("downarrow").hidden = true;
+        //   this.onfirstscroll = false;
+        // }
+        this.onscroll = false;
       }
 
-      if(this.timer !== null) {
-        clearTimeout(this.timer);        
+      if (this.timer !== null) {
+        clearTimeout(this.timer);
       }
       this.timer = setTimeout(() => {
-        this.onscroll=true;
-        this.setState({"active": "off"})
+        this.onscroll = true;
+        this.setState({"active" : "off"});
       }, 150);
-      // el.style.animationPlayState = "running";
+      this.notfirstreloadscroll = true;
     }, true);
     this.loop.addEventListener("wheel", (e) => e.stopPropagation(), true);
     this.loop.scrollTop = 1; // to allow upwards scrolling
     this.scrollHeight = this.loop.scrollHeight;
+    console.log(this.scrollHeight);
     this.disableScrollCheck = false;
   }
   scrollCheck() {
@@ -82,6 +88,11 @@ class Loopcontent extends React.Component {
     }
   }
 
+  scrollToNextDiv(id){
+    let nextdiv = document.getElementById(id);
+    nextdiv.scrollIntoView({behavior: "smooth"});
+  }
+
   render() {
     // window.setInterval(function() {
     //   this.loop.scrollTop += 3;
@@ -90,43 +101,54 @@ class Loopcontent extends React.Component {
         <div id="loopcontent" onMouseLeave =
              {() => globalstateobj.mouseOver = false} onMouseOver =
                  {() => globalstateobj.mouseOver = true} className={`${this.state.active === "on" ? "animate-on" : "animate-off"}`}>
-          <div style={{ height: "3600px" }}>
-            <div style={{height: "720px" }}>
+          <div style={{
+      height: "3600px" }}>
+            <div style={{
+      height: "620px" }}>
               <br />
               <br />
               <h1>Welcome to my</h1>
             <h1>World</h1>
-              <p>
-              </p>
+            <img onClick={() => this.scrollToNextDiv("cont2")} src={require("../resources/downarrow.png")} className="downarrow" alt="Down"/>
             </div>
-            <div style={{height: "720px" }}>
+            <div style={{height: "820px" }} id="cont2">
+              <br />
+              <br />
               <h1>Some Music</h1>
             <p>To enjoy the ride on my site you can listen to chill spacey
                 music. Feel free to press play:)</p>
+            <img onClick={() => this.scrollToNextDiv("cont3")} src={require("../resources/downarrow.png")} className="downarrow" alt="Down"/>
             </div>
            <div style = {{
-      height: "720px" }}><br /><br />
+      height: "720px" }} id="cont3"><br /><br />
            <h1>My Workspace</h1>
+            <p>If you don't have great sound, nice lights and a powerfull PC, get it now!</p>
+            <img onClick={() => this.scrollToNextDiv("cont4")} src={require("../resources/downarrow.png")} className="downarrow" alt="Down"/>
             </div>
            <div style = {{
-      height: "580px" }}><br /><br />
+      height: "640px" }} id="cont4"><br /><br />
            <h1>Things I find</h1>
               <h1>interesting</h1>
+            <img onClick={() => this.scrollToNextDiv("cont5")} src={require("../resources/downarrow.png")} className="downarrow" alt="Down"/>
            </div>
-            <div style={{height: "860px" }}>
-              <br/>
-           <br /><h1>Ty &lt;3</h1>
+            <div style={{height: "800px" }} id="cont5">
+           <h1>Ty &lt;3</h1>
               <ImmortalizerForm />
+              <br />
+              <br />
+              <br />
+              <br />
+            <p>Back to the beginning</p>
+            <img onClick={() => this.scrollToNextDiv("cont1")} src={require("../resources/downarrow.png")} className="downarrow" alt="Down"/>
             </div>
           </div>
-          <div className="isclone" style={{
+          <div className="isclone" id="cont1" style={{
       height: "720px" }}>
               <br />
               <br />
               <h1>Welcome to my</h1>
               <h1>World</h1>
-              <p>
-              </p>
+              <img onClick={() => this.scrollToNextDiv("cont2")} src={require("../resources/downarrow.png")} className="downarrow" alt="Down"/>
             </div>
         </div>
       </>
