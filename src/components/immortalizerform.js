5f869b560aca89195d460d64b631b4ac9e40c498
Leo310 <leodev310@gmail.com>
2022-10-11 10:46:48 +0200

    Update: wordcloud api url. same ip on same k8s pod

--- src/components/immortalizerform.js
+++ src/components/immortalizerform.js
@@ -14,7 +14,7 @@ class ImmortalizerForm extends React.Component {
   handleChange(event) { this.setState({value : event.target.value}); }
 
   async fetchWordcloud() {
-    let res = await fetch("http://192.168.1.9:5000", {
+    let res = await fetch("http://0.0.0.0:3001", {
       method : 'POST',
       headers :
           {'Accept' : 'application/json', 'Content-Type' : 'application/json'},
